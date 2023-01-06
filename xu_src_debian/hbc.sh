af8f21c0d54f21ca8986410838b75b0ccbfb48fd
Mr. Dalien <daniel.stuardo@gmail.com>
2023-01-05 22:39:15 -0300

    Add files via upload

--- xu_src_debian/hbc.sh
+++ xu_src_debian/hbc.sh
@@ -5,11 +5,26 @@
 #-D_MAC64_
 #/opt/harbour/bin/hbmk2  -lhbct -lhbmisc -blinker -nodebug  -D_MAC64_ -static xuc
 
-if [ "$1" == "C" ]; then
+if [ "$1" == "S" ]; then
    hbmk2 -lhbct -lhbmisc -D_LINUX64_ -static xu
-   hbmk2 -lhbct -lhbmisc -D_LINUX64_ -static xuc
+   hbmk2 -lhbct -lhbmisc -lhbct -D_LINUX64_ -static xuc
+   hbmk2 -lhbct -static laica
 fi
-hbmk2 -lhbct -static ed4xu
+
+if [ "$1" == "M" ]; then
+   hbmk2 -lhbct -lhbmisc -D_LINUX64_ -static xu
+fi
+if [ "$1" == "C" ]; then
+   echo "COMPILANDO XUC..."
+   hbmk2 -lhbct -lhbmisc -lhbct -D_LINUX64_ -static xuc
+fi
+if [ "$1" == "V" ]; then
+   hbmk2 -lhbct -lhbmisc -D_LINUX64_ -static xu
+   hbmk2 -lhbct -lhbmisc -lhbct -D_LINUX64_ -static xuc
+fi
+if [ "$1" == "E" ]; then
+   hbmk2 -lhbct -static laica
+fi
 
 #/opt/harbour/bin/hbmk2   -lhbmisc -blinker -nodebug  -D_MAC64_ -static xuc
 #/opt/harbour/bin/hbmk2   -lhbmisc -blinker -nodebug  -D_MAC64_ -static xup
@@ -18,10 +33,28 @@ hbmk2 -lhbct -static ed4xu
 #mv xucr xuc
 #mv xup xu
 
-if [ "$1" == "C" ]; then
+#if [ "$1" == "S" ]; then
+#   sudo cp xu /usr/bin/xu
+#   sudo cp xuc /usr/bin/xuc
+#   sudo cp laica /usr/bin/laica
+#fi
+if [ "$1" == "V" ]; then
    sudo cp xu /usr/bin/xu
    sudo cp xuc /usr/bin/xuc
 fi
-sudo cp ed4xu /usr/bin/ed4xu
+if [ "$1" == "C" ]; then
+   echo "COMPIANDO XU Y XUC..."
+   sudo cp xu /usr/bin/xu
+   sudo cp xuc /usr/bin/xuc
+   exit 0
+fi
+if [ "$1" == "M" ]; then
+   sudo cp xu /usr/bin/xu
+   exit 0
+fi
+if [ "$1" == "E" ]; then
+   sudo cp laica /usr/bin/laica
+   exit 0
+fi
 
 exit 0
